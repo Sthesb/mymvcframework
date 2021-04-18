9f1acd184fb712c3454fc491b9be90c2d81ef66d
Sthembiso Mathebula <41741561+Sthesb@users.noreply.github.com>
2021-04-18 12:11:40 +0200

    Delete app directory

--- app/Require.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // Require libraries from the folder Libraries
-    require_once 'Libraries/Core.php';
-    require_once 'Libraries/Controller.php';
-    require_once 'Libraries/Database.php';
-    require_once 'Config/Config.php';
-
-    // Instantiate core class 
-    $init = new Core();
